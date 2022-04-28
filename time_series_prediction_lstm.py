2a3e0cd22ea3924d0c6a48180a0934dfc5cb7910
jflics@umich.edu <jflics@umich.edu>
2022-04-28 16:38:31 -0400

    Update LSTM data pipeline

--- time_series_prediction_lstm.py
+++ time_series_prediction_lstm.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing, model_selection
 from keras import models, layers
+from matplotlib import pyplot as plt
 """
         Input and output variables to choose from:
         Tboil: boiler temperature at each time, K
@@ -20,8 +21,13 @@ from keras import models, layers
         rho2: water density at turbine outlet, kg/m^3
 """
 
+def load_data_npy(input_variables, output_variable):
+    training_data = pd.DataFrame()
+    for feature in [output_variable] + input_variables:
+        training_data[feature] = pd.DataFrame(np.load('lstm_data/' + feature + '_log.csv.npy'))
+    return training_data
 
-# TODO: add choice of row
+
 def load_data(input_variables, output_variable):
     training_data = pd.DataFrame()
     for feature in [output_variable] + input_variables:
@@ -39,9 +45,9 @@ if __name__ == "__main__":
     output_variable = 'Eturb'
 
     # Loads df with input columns followed by output column
-    training_data = load_data(input_variables, output_variable)
-
-    # TODO: Test/Training split
+    training_data = load_data_npy(input_variables, output_variable)
+    Y_plot = training_data[output_variable]
+    Y_plot = np.array(Y_plot)
     # Normalizes input data
     normalizer = preprocessing.StandardScaler()
     normalizer = normalizer.fit(training_data)
@@ -56,7 +62,7 @@ if __name__ == "__main__":
     X, Y = np.array(X), np.array(Y)
 
     # Splits data into testing and training set
-    test_size = 0.33
+    test_size = 0.35
     X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=test_size, shuffle=False)
 
     # Now that data is prepared, we can make model
@@ -66,14 +72,29 @@ if __name__ == "__main__":
     prone to it.
     '''
     model = models.Sequential()
-    model.add(layers.LSTM(32, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2]), return_sequences=False))
+    model.add(layers.LSTM(64, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2]), return_sequences=False))
+    #model.add(layers.LSTM(32, activation='relu', return_sequences=False))
     model.add(layers.Dropout(0.2))
     model.add(layers.Dense(1))
     model.compile(optimizer='adam', loss='mse')
     print(model.summary())
-    hist = model.fit(X_train, Y_train, epochs=100, batch_size=25, validation_split=0.1, verbose=1)
+    hist = model.fit(X_train, Y_train, epochs=50, batch_size=25, validation_split=0.1, verbose=1)
 
     forecast_normalized = model.predict(X_test)
     forecast_normalized_repeat = np.repeat(forecast_normalized, training_data.shape[1], axis=-1)
     forecast = normalizer.inverse_transform(forecast_normalized_repeat)[:, 0]
-    # TODO: add graph/print out metrics
+    eval = model.evaluate(X_test, Y_test)
+    print(eval)
+
+    Y_test = Y_plot[-Y_test.shape[0]:]
+
+    plot_num = 5000
+    x_axis = [i for i in range(plot_num)]
+    #plt.plot(x_axis, forecast_normalized[:plot_num])
+    plt.plot(x_axis, Y_test[:plot_num], 'b-', label="Actual")
+    plt.plot(x_axis, forecast[:plot_num], 'r-', label="Predicted")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Energy (kJ)")
+    plt.title("Turbine Energy vs Time")
+    plt.legend()
+    plt.show()
